6033e125fe8b2413e9c857cdc45910c50ac9551f
Eysteinn Hrafnkelsson <eysteinnh19@ru.is>
2020-09-17 15:47:18 +0000

    hehe

--- Tile_Traveller.py
+++ Tile_Traveller.py
@@ -1,28 +1,53 @@
 ###### Tile Travel
 
+## map 3x3
 
-## map
+def movement(movemnt_str, x, y):
+    if movemnt_str == 'n':
+        loc_y += 1
+    elif movemnt_str == 's':
+        loc_y -= 1
+    elif movemnt_str == 'w':
+        loc_x -= 1
+    elif movemnt_str == 'e':
+        loc_x += 1
+    return loc_x, loc_y
 
-# tile ; valmöguleikar
-# 1,1 ; N/upp
-# 1,2 ; N/upp , E/right
-# 1,3 : S/down , E/right
-# 2,3 : W/left , E/right
-# 2,2 : w/left , S/down
-# 2,1 : N/upp
-# 3,3 ; w/left , S/down
-# 3,2 : N/upp , S/down
-# 3,3 ; N/upp
+
+## Tiles (movement possibilities)
+Tile1_1 = '(N)orth.'
+Tile1_2 = '(N)orth or (E)ast or (S)outh.'
+Tile1_3 = '(E)ast or (S)outh.'
+Tile2_1 = '(N)orth.'
+Tile2_2 = '(S)outh or (W)est'
+Tile2_3 = '(E)ast or (W)est'
+Tile3_2 = '(N)orth or (S)outh.
+Tile3_3 = '(S)outh or (W)est'
+
 
 ## beginning
-
-  
-
-
-
+loc_x, loc_y = 1,1
+move = input("Direction: ").lower()
+current_loc = Tile1_1
 
 
-
+## Tiles location
+if loc_x == 1 and loc_y == 1:
+    print(Tile1_1)
+elif loc_x == 1 and loc_y == 2:
+    print(Tile1_2)
+elif loc_x == 1 and loc_y == 3:
+    print(Tile1_3)
+elif loc_x == 2 and loc_y == 1:
+    print(Tile2_1)
+elif loc_x == 2 and loc_y == 2:
+    print(Tile2_2)
+elif loc_x == 2 and loc_y == 3:
+    print(Tile2_3)
+elif loc_x == 3 and loc_y == 2:
+    print(Tile3_2)
+elif loc_x == 3 and loc_y == 3:
+    print(Tile3_3)
 
 
 
