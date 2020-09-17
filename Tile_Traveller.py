405669c18ad12395b51493b2466a65858c473d78
Eysteinn Hrafnkelsson <eysteinnh19@ru.is>
2020-09-17 17:02:18 +0000

    Lang á leið komið

--- Tile_Traveller.py
+++ Tile_Traveller.py
@@ -2,18 +2,16 @@
 
 ## map 3x3
 
-def movement(movemnt_str):
-    loc_x = 1
-    loc_y = 1
+def movement(movemnt_str, x, y):
     if movemnt_str == 'n':
-        loc_y += 1
+        y += 1
     elif movemnt_str == 's':
-        loc_y -= 1
+        y -= 1
     elif movemnt_str == 'w':
-        loc_x -= 1
+        x -= 1
     elif movemnt_str == 'e':
-        loc_x += 1
-    return loc_x, loc_y
+        x += 1
+    return x, y
 
 
 ## Tiles (movement possibilities)
@@ -23,39 +21,56 @@ Tile1_3 = '(E)ast or (S)outh.'
 Tile2_1 = '(N)orth.'
 Tile2_2 = '(S)outh or (W)est'
 Tile2_3 = '(E)ast or (W)est'
+Tile3_1 = "Victory!"
 Tile3_2 = '(N)orth or (S)outh.'
 Tile3_3 = '(S)outh or (W)est'
 
 loc_x, loc_y = 1,1
-move = input("Direction: ")
+print(Tile1_1)
+move = input("Direction: ").lower()
 
-eysteinn = 'hóra'
+eysteinn = 'news'
 ## beginning
-while eysteinn == 'hóra':
+while move in eysteinn:
     current_loc = Tile1_1
-    u = movement(move)
+    u = movement(move, loc_x, loc_y)
     loc_x = u[0]
     loc_y = u[1]
 
-    print(loc_x,loc_y)
+    print(loc_x,loc_y) #Til þess að sjá staðsetningu
 
     ## Tiles location
     if loc_x == 1 and loc_y == 1:
+        current_loc = Tile1_1
         print(Tile1_1)
     elif loc_x == 1 and loc_y == 2:
+        current_loc = Tile1_2        
         print(Tile1_2)
     elif loc_x == 1 and loc_y == 3:
+        current_loc = Tile1_3
         print(Tile1_3)
     elif loc_x == 2 and loc_y == 1:
+        current_loc = Tile2_1
         print(Tile2_1)
     elif loc_x == 2 and loc_y == 2:
+        current_loc = Tile2_2
         print(Tile2_2)
     elif loc_x == 2 and loc_y == 3:
+        current_loc = Tile2_3
         print(Tile2_3)
     elif loc_x == 3 and loc_y == 2:
+        current_loc = Tile3_2
         print(Tile3_2)
     elif loc_x == 3 and loc_y == 3:
+        current_loc = Tile3_3
         print(Tile3_3)
-    move = input("Direction: ")
+    elif loc_x == 3 and loc_y == 1:
+        print(Tile3_1)
+        break
 
+    move = input("Direction: ")
+else:
+    print("Not a valid direction!")
+    #print(current_loc)
+    move = input("Direction: ")
 
