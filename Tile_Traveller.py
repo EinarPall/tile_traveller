1c75266179230e18519d746510d44043af8e0f95
Eysteinn Hrafnkelsson <eysteinnh19@ru.is>
2020-09-17 18:28:06 +0000

    Einar

--- Tile_Traveller.py
+++ Tile_Traveller.py
@@ -11,6 +11,8 @@ def movement(movemnt_str, x, y):
         x -= 1
     elif movemnt_str == 'e'and 'E' in current_loc:
         x += 1
+    else:
+        print("Not a valid direction!")
     return x, y
 
 
@@ -29,52 +31,51 @@ loc_x, loc_y = 1,1
 print(Tile1_1)
 move = input("Direction: ").lower()
 current_loc = Tile1_1
-eysteinn = 'news'
+direction = 'news'
 ## beginning
-while move in eysteinn:
+
+
+while move in direction:
     u = movement(move, loc_x, loc_y)
     loc_x = u[0]
     loc_y = u[1]
 
-    print(loc_x,loc_y) #Til þess að sjá staðsetningu
-
+    #print(loc_x,loc_y) #Til þess að sjá staðsetningu
+    tra = "You can travel:"
     ## Tiles location
     if loc_x == 1 and loc_y == 1:
         current_loc = Tile1_1
-        print(Tile1_1)
+        print(tra,Tile1_1)
     elif loc_x == 1 and loc_y == 2:
         current_loc = Tile1_2        
-        print(Tile1_2)
+        print(tra,Tile1_2)
     elif loc_x == 1 and loc_y == 3:
         current_loc = Tile1_3
-        print(Tile1_3)
+        print(tra,Tile1_3)
     elif loc_x == 2 and loc_y == 1:
         current_loc = Tile2_1
-        print(Tile2_1)
+        print(tra,Tile2_1)
     elif loc_x == 2 and loc_y == 2:
         current_loc = Tile2_2
-        print(Tile2_2)
+        print(tra,Tile2_2)
     elif loc_x == 2 and loc_y == 3:
         current_loc = Tile2_3
-        print(Tile2_3)
+        print(tra,Tile2_3)
     elif loc_x == 3 and loc_y == 2:
         current_loc = Tile3_2
-        print(Tile3_2)
+        print(tra,Tile3_2)
     elif loc_x == 3 and loc_y == 3:
         current_loc = Tile3_3
-        print(Tile3_3)
+        print(tra,Tile3_3)
     elif loc_x == 3 and loc_y == 1:
-        print(Tile3_1)
+        print(tra,Tile3_1)
         break
     else: 
         print("Not a valid direction!")
-        # loc_y = int(current_loc[:-1]) 
-        # loc_x = int(current_loc[:-3])
-        
-    move = input("Direction: ")
+        print(current_loc)
+    move = input("Direction: ").lower()
 else:
     print("Not a valid direction!")
-    #print(current_loc)
-    move = input("Direction: ")
-
+    print(current_loc)
+    move = input("Direction: ").lower()
 
